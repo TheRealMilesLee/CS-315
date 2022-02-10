fa6a4947b8a4accb85a8e22ffa7d68e189c967a5
Serendipity <1789216340lhy@gmail.com>
2022-02-10 15:34:43 -0600

    new file:   Week5-PHP/.idea/.gitignore 	new file:   Week5-PHP/.idea/Week5-PHP.iml 	new file:   Week5-PHP/.idea/modules.xml 	new file:   Week5-PHP/.idea/php.xml 	new file:   Week5-PHP/.idea/vcs.xml 	new file:   Week5-PHP/Quiz.php 	modified:   Week5-PHP/managewords.php 	new file:   Week5-PHP/quiz.css 	new file:   Week5-PHP/words.txt

--- Week5-PHP/managewords.php
+++ Week5-PHP/managewords.php
@@ -1,4 +1,15 @@
-<?php phpinfo(); ?>
+<?php
+/**
+ * @description A program to manage the word list for the GRE quiz
+ * This program has two functions
+ * 1. Add a word
+ * 2. Delete a word from the current list
+ * @author Hengyi Li
+ * @copyright Copyright (c). 2022 Hengyi Li. All rights reserved
+ * @version 0.0.1 Alpha
+ */
+const DEFINITION_FILENAME = 'words.txt';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,4 +18,15 @@
     <title> Manage Words </title>
     <meta name="author" content="Hengyi Li" />
   </head>
-
+  <body>
+    <h1> Word Manager </h1>
+    <p> This is the Zelda's GRE vocabulary manager, choose to add a new word or delete a word.</p>
+    <label>
+        <select>
+            <option value="default"> Make a selection </option>
+            <option value="Add"> Add a new word </option>
+            <option value="Delete"> Delete a word </option>
+        </select>
+    </label>
+  </body>
+</html>
