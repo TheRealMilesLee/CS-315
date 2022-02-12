1420c5a8c89764e683cba15c85e0336397f7d9cf
Serendipity <1789216340lhy@gmail.com>
2022-02-12 14:25:37 -0600

    new file:   Week5-PHP/1.txt 	deleted:    Week5-PHP/Quiz.php 	deleted:    Week5-PHP/echo.php 	modified:   Week5-PHP/manage_words.php 	deleted:    Week5-PHP/quiz.css 	deleted:    Week5-PHP/words.txt Change the file status and reformatted the file. Now problem in on the fixing the post and get

--- Week5-PHP/manage_words.php
+++ Week5-PHP/manage_words.php
@@ -8,7 +8,7 @@
  * @copyright Copyright (c). 2022 Hengyi Li. All rights reserved
  * @version 0.0.1 Alpha
  */
-const DEFINITION_FILENAME = 'words.txt';
+define('DEFINITION_FILENAME', '1.txt');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -23,28 +23,45 @@ const DEFINITION_FILENAME = 'words.txt';
   This is the Zelda's GRE vocabulary manager, choose to add a new word or
   delete a word.
 </p>
+
 <h2> Add a new word </h2>
-<form method="post" action="echo.php">
+<form method="post" action="manage_words.php">
   <p>
-    What's the word?
+    <label for="new_word"> What's the word? </label>
     <input type='text' id='new_word' name='new_word' required />
   </p>
   <p>
-    What's the part of speech of the word?
+    <label for="speech"> What's the part of speech of the word? </label>
     <input type='text' id="speech" name="speech" required />
   </p>
   <p>
-    What's the definition of the word?
+    <label for="def_new_word"> What's the definition of the word? </label>
     <input type='text' id="def_new_word" name="def_new_word" required />
   </p>
+  <input type='submit' value='Submit Report' name='submit' />
 </form>
+<?php
+if (isset($_POST) && isset($_POST['new_word'])
+  && preg_match('|^[A-Za-z]+$|', $_POST['new_word'])
+  && isset($_POST['def_new_word'])
+  && preg_match('|^[A-Za-z;( -]+|', $_POST['def_new_word'])
+  && isset($_POST['speech'])
+  && preg_match('|^[a-z]+.$|', $_POST['speech']))
+{
+  $first_time = false;
+  $word_new = $_POST['new_word'];
+  $part_speech = $_POST['speech'];
+  $definition = $_POST['def_new_word'];
+  $msg = "$word_new has been added with the part of speech is $part_speech and definition is $definition \n";
+  file_put_contents(DEFINITION_FILENAME, $msg, LOCK_EX | FILE_APPEND);
+}
+?>
 <h2> Delete a word </h2>
 <form method="post" action="manage_words.php">
   <p>
     What's the word?
-    <input type='text' id='new_word' name='new_word' required />
+    <input type='text' id='del_word' name='del_word' required />
   </p>
 </form>
-<input type='submit' value='Submit Report' name='submit' />
 </body>
 </html>
