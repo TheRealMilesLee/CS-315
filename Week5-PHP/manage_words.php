6f850738a815ac84fd9a78a9b4ee42e3b5aec91d
Serendipity <1789216340lhy@gmail.com>
2022-02-13 00:09:38 -0600

    modified:   Week5-PHP/1.txt 	modified:   Week5-PHP/2.txt 	modified:   Week5-PHP/3.txt 	modified:   Week5-PHP/manage_words.php 	modified:   Week5-PHP/results.txt Changed the test file

--- Week5-PHP/manage_words.php
+++ Week5-PHP/manage_words.php
@@ -8,7 +8,41 @@
  * @copyright Copyright (c). 2022 Hengyi Li. All rights reserved
  * @version 0.0.1 Alpha
  */
-define('DEFINITION_FILENAME', '1.txt');
+define('DEFINITION_FILENAME', 'results.txt');
+
+function delete_word($file_to_load, $delete_word)
+{
+  $paired_array = array();
+  $total_lines = file($file_to_load, FILE_IGNORE_NEW_LINES);
+  for ($loop = 0;$loop < count($total_lines); $loop++)
+  {
+    list($word, $part, $definition) = explode("\t", $total_lines[$loop]);
+    $combined = "$part \t $definition";
+    $paired_array[] = array($word => $combined);
+  }
+  unset($paired_array[$delete_word]);
+  print_r($paired_array);
+}
+
+function validate($file_to_load, $word_new, $input_result)
+{
+  $paired_array = array();
+  $total_lines = file($file_to_load, FILE_IGNORE_NEW_LINES);
+  for ($loop = 0;$loop < count($total_lines); $loop++)
+  {
+    list($word, $part, $definition) = explode("\t", $total_lines[$loop]);
+    $combined = "$part \t $definition";
+    $paired_array[] = array($word => $combined);
+  }
+  if (is_numeric(array_search($word_new,$paired_array)))
+  {
+    echo 'The word is already exist!';
+  }
+  else
+  {
+    file_put_contents('3.txt', $input_result, LOCK_EX | FILE_APPEND);
+  }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -23,7 +57,6 @@ define('DEFINITION_FILENAME', '1.txt');
   This is the Zelda's GRE vocabulary manager, choose to add a new word or
   delete a word.
 </p>
-
 <h2> Add a new word </h2>
 <form method="post" action="manage_words.php">
   <p>
@@ -32,7 +65,12 @@ define('DEFINITION_FILENAME', '1.txt');
   </p>
   <p>
     <label for="speech"> What's the part of speech of the word? </label>
-    <input type='text' id="speech" name="speech" required />
+    <select name="speech" id="speech">
+      <option value='noun'> noun </option>
+      <option value='adjective'> adjective </option>
+      <option value='adverb'> adverb </option>
+      <option value='verb'> verb </option>
+    </select>
   </p>
   <p>
     <label for="def_new_word"> What's the definition of the word? </label>
@@ -48,20 +86,28 @@ if (isset($_POST) && isset($_POST['new_word'])
   && isset($_POST['speech'])
   && preg_match('|^[a-z]+.$|', $_POST['speech']))
 {
-  $first_time = false;
   $word_new = $_POST['new_word'];
   $part_speech = $_POST['speech'];
   $definition = $_POST['def_new_word'];
-  $msg = "$word_new has been added with the part of speech is $part_speech and definition is $definition \n";
-  file_put_contents(DEFINITION_FILENAME, $msg, LOCK_EX | FILE_APPEND);
+  $input_result = "$word_new \t $part_speech \t $definition \n";
+  validate(DEFINITION_FILENAME, $word_new, $input_result);
 }
 ?>
 <h2> Delete a word </h2>
 <form method="post" action="manage_words.php">
   <p>
-    What's the word?
+    <label for="del_word"> What's the word? </label>
     <input type='text' id='del_word' name='del_word' required />
   </p>
+  <input type='submit' value='Delete option' name='submit_delete_word'/>
 </form>
+<?php
+if (isset($_POST) && isset($_POST['del_word'])
+  && preg_match('|^[A-Za-z]+$|', $_POST['del_word']))
+{
+  $deleted_word = $_POST['del_word'];
+  delete_word(DEFINITION_FILENAME, $deleted_word);
+}
+?>
 </body>
 </html>
