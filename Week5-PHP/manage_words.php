523c1c56689d9da5bfa9cc42ac463933ef45795e
Serendipity <1789216340lhy@gmail.com>
2022-02-13 15:29:27 -0600

    new file:   Week5-PHP/1.txt 	new file:   Week5-PHP/filename.txt 	modified:   Week5-PHP/manage_words.php Change the output to file

--- Week5-PHP/manage_words.php
+++ Week5-PHP/manage_words.php
@@ -9,49 +9,78 @@
  * @version 0.0.1 Alpha
  */
 define('DEFINITION_FILENAME', 'results.txt');
+/**
+ * @param array $paired_array is the array to output
+ */
+function output_to_file(array $paired_array): void
+{
+  for ($index = 0; $index < count($paired_array); $index++)
+  {
+    $words_total = $paired_array[$index]['word'];
+    $part_of_speech_total = $paired_array[$index]['part'];
+    $definition_total = $paired_array[$index]['definition'];
+    $result = "$words_total\t$part_of_speech_total\t$definition_total\n";
+    file_put_contents('filename.txt', $result, LOCK_EX | FILE_APPEND);
+  }
+}
+
+function search_word($array, $word_to_search): int
+{
+  $index = 0;
+  $found = false;
+  $position = -1;
+  while ($index < count($array) && !$found)
+  {
+    if ($array[$index]['word'] == $word_to_search)
+    {
+      $position = $index;
+      $found = true;
+    }
+    $index++;
+  }
+  unset($index);
+  return $position;
+}
 
 /**
  * This function is to read the file and make a word-speech and definition
  * pair.
- * @param $file_to_load is the file to load from disk
+ * @param $file_to_load
  * @return array is the key-value paired word list
  */
 function getPaired_array($file_to_load): array
 {
   $paired_array = array();
   $total_lines = file($file_to_load, FILE_IGNORE_NEW_LINES);
+  ksort($total_lines);
   for ($loop = 0; $loop < count($total_lines); $loop++)
   {
     list($word, $part, $definition) = explode("\t", $total_lines[$loop]);
-    $combined = "$part \t $definition";
-    $paired_array[] = array($word => $combined);
+    $paired_array[] =
+      array('word' => $word, 'part' => $part, 'definition' => $definition);
   }
   return $paired_array;
 }
 
 /**
  * This function is to delete the word
- * @param $file_to_load is the file that comes from disk
- * @param $delete_word is the word that user wants to delete
+ * @param $file_to_load
+ * @param $delete_word
  */
 function delete_word($file_to_load, $delete_word)
 {
   $paired_array = getPaired_array($file_to_load);
-  unset($paired_array[$delete_word]);
-  file_put_contents('filename.txt', print_r($paired_array, true));
-}
-
-function validate($file_to_load, $word_new, $input_result)
-{
-  $paired_array = getPaired_array($file_to_load);
-  if (is_numeric(array_search($word_new,$paired_array)))
+  $position = search_word($paired_array, $delete_word);
+  if ($position > -1)
   {
-    echo 'The word is already exist!';
+    unset($paired_array[$position]);
   }
   else
   {
-    file_put_contents(DEFINITION_FILENAME, $input_result, LOCK_EX | FILE_APPEND);
+    echo "No entries found! ";
   }
+  ksort($paired_array);
+  output_to_file($paired_array);
 }
 ?>
 <!DOCTYPE html>
@@ -76,31 +105,43 @@ function validate($file_to_load, $word_new, $input_result)
   <p>
     <label for="speech"> What's the part of speech of the word? </label>
     <select name="speech" id="speech">
-      <option value='noun'> noun </option>
-      <option value='adjective'> adjective </option>
-      <option value='adverb'> adverb </option>
-      <option value='verb'> verb </option>
+      <option value='noun'> noun</option>
+      <option value='adjective'> adjective</option>
+      <option value='adverb'> adverb</option>
+      <option value='verb'> verb</option>
     </select>
   </p>
   <p>
     <label for="def_new_word"> What's the definition of the word? </label>
     <input type='text' id="def_new_word" name="def_new_word" required />
   </p>
-  <input type='submit' value='Submit Report' name='submit' />
+  <p>
+    <input type='submit' value='Submit Report' name='submit' />
+  </p>
 </form>
 <?php
-if (isset($_POST) && isset($_POST['new_word'])
+$paired_array = getPaired_array(DEFINITION_FILENAME);
+if ( isset($_POST) && isset($_POST['new_word'])
   && preg_match('|^[A-Za-z]+$|', $_POST['new_word'])
   && isset($_POST['def_new_word'])
-  && preg_match('|^[A-Za-z;( -]+|', $_POST['def_new_word'])
-  && isset($_POST['speech'])
-  && preg_match('|^[a-z]+.$|', $_POST['speech']))
+  && preg_match('|^[A-Za-z;( -]+|', $_POST['def_new_word']))
 {
   $word_new = $_POST['new_word'];
   $part_speech = $_POST['speech'];
   $definition = $_POST['def_new_word'];
-  $input_result = "$word_new \t $part_speech \t $definition \n";
-  validate(DEFINITION_FILENAME, $word_new, $input_result);
+  $lowercase_word = strtolower($word_new);
+  $position = search_word($paired_array, $word_new);
+  if ($position > -1)
+  {
+    echo 'The word is already exist!';
+  }
+  else
+  {
+    $paired_array[] =
+      array('word' => $lowercase_word, 'part' => $part_speech, 'definition' => $definition);
+    ksort($paired_array);
+    output_to_file($paired_array);
+  }
 }
 ?>
 <h2> Delete a word </h2>
@@ -109,10 +150,10 @@ if (isset($_POST) && isset($_POST['new_word'])
     <label for="del_word"> What's the word? </label>
     <input type='text' id='del_word' name='del_word' required />
   </p>
-  <input type='submit' value='Delete option' name='submit_delete_word'/>
+  <input type='submit' value='Delete option' name='submit_delete_word' />
 </form>
 <?php
-if (isset($_POST) && isset($_POST['del_word'])
+if ( isset($_POST) && isset($_POST['del_word'])
   && preg_match('|^[A-Za-z]+$|', $_POST['del_word']))
 {
   $deleted_word = $_POST['del_word'];
