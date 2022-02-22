c91312e219b5b9edb71ba278719b42754bcc70a8
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-02-22 15:47:59 -0600

    ·

--- Week6-PHPEnhanced/managewords.php
+++ Week6-PHPEnhanced/managewords.php
@@ -1,160 +1,174 @@
 <?php
-  /**
-   * @description A program to manage the word list for the GRE quiz
-   * This program has two functions
-   * 1. Add a word
-   * 2. Delete a word from the current list
-   * @author Hengyi Li
-   * @copyright Copyright (c). 2022 Hengyi Li. All rights reserved
-   * @version 10.2.13 Release
-   */
 
-  define("DEFINITION_FILENAME", "words.txt");
-  error_reporting(E_ALL);
-  ini_set('display_errors', '1');
+/**
+ * @description A program to manage the word list for the GRE quiz
+ * This program has two functions
+ * 1. Add a word
+ * 2. Delete a word from the current list
+ * @author Hengyi Li
+ * @copyright Copyright (c). 2022 Hengyi Li. All rights reserved
+ * @version 10.2.13 Release
+ */
 
-  /**
-   * This function is to output the contents to the file
-   * @param array $paired_array is the array to output
-   */
-  function output_to_file(array $paired_array): void
+define("DEFINITION_FILENAME", "words.txt");
+define("PART_OF_SPEECH", "part.txt");
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+/**
+ * This function is to output the contents to the file
+ * @param array $paired_array is the array to output
+ */
+function output_to_file(array $paired_array): void
+{
+  // Sort the array in ascending order
+  array_multisort($paired_array, SORT_ASC, SORT_REGULAR);
+  // cleanup original content for new input
+  file_put_contents(DEFINITION_FILENAME, '');
+  $index = 0;
+  while ($index < count($paired_array))
   {
-    // Sort the array in ascending order
-    array_multisort($paired_array, SORT_ASC, SORT_REGULAR);
-    // cleanup original content for new input
-    file_put_contents(DEFINITION_FILENAME, '');
-    $index = 0;
-    while ($index < count($paired_array))
+    // Jump the hole in the array
+    if (!array_key_exists($index, $paired_array))
     {
-      // Jump the hole in the array
-      if (!array_key_exists($index, $paired_array))
-      {
-        $index++;
-      }
-      $words_total = $paired_array[$index]['word'];
-      $part_of_speech_total = $paired_array[$index]['part'];
-      $definition_total = $paired_array[$index]['definition'];
-      $result = "$words_total\t$part_of_speech_total\t$definition_total\n";
-      file_put_contents(DEFINITION_FILENAME, $result, LOCK_EX | FILE_APPEND);
       $index++;
     }
-    unset($paired_array[$index]);
+    $words_total = $paired_array[$index]['word'];
+    $part_of_speech_total = $paired_array[$index]['part'];
+    $definition_total = $paired_array[$index]['definition'];
+    $result = "$words_total\t$part_of_speech_total\t$definition_total\n";
+    file_put_contents(DEFINITION_FILENAME, $result, LOCK_EX | FILE_APPEND);
+    $index++;
   }
+  unset($paired_array[$index]);
+}
 
 
 /**
  * This function is to display the currently entry
  * @param string $file_to_load is the file name from  the file
  */
-function display ( string $file_to_load)
+function display(string $file_to_load)
 {
-    $paired_array = getPaired_array($file_to_load);
-    array_multisort($paired_array, SORT_ASC, SORT_REGULAR);
-    for ($index = 0; $index < count($paired_array); $index++)
+  $paired_array = getPaired_array($file_to_load);
+  array_multisort($paired_array, SORT_ASC, SORT_REGULAR);
+  for ($index = 0; $index < count($paired_array); $index++)
+  {
+    if (!array_key_exists($index, $paired_array))
     {
-      if (!array_key_exists($index, $paired_array))
-      {
-        $index++;
-      }
-      $words_total = $paired_array[$index]['word'];
-      $part_of_speech_total = $paired_array[$index]['part'];
-      $definition_total = $paired_array[$index]['definition'];
-      $result = "$words_total\t$part_of_speech_total\t$definition_total\n";
-    ?>
+      $index++;
+    }
+    $words_total = $paired_array[$index]['word'];
+    $part_of_speech_total = $paired_array[$index]['part'];
+    $definition_total = $paired_array[$index]['definition'];
+    $result = "$words_total\t$part_of_speech_total\t$definition_total\n";
+?>
     <p class="selection_to_delete_style">
       <input type="checkbox" name="choice_to_delete[]" value="<?= $index ?>" />
       <label for="choice_to_delete"> <?= $index ?> </label>
-      <?= " : ", $result, "<br />"  ?>
+      <?= " : ", $result, "<br />" ?>
     </p>
-    <?php
+  <?php
   }
 }
-  /**
-   * @param array $array is the array to sort
-   * @param string $word_to_search is the word that search in the array
-   * @return int is the index of the word
-   */
-  function search_word(array &$array, string $word_to_search, $part_of_speech): int
-  {
-    $found = false;
-    $index = 0;
-    while ($index < count($array) && !$found)
-    {
-      if (strcmp($array[$index]['word'], $word_to_search) == 0 &&
-        strcmp($array[$index]['part'],$part_of_speech) == 0)
-      {
-        $found = true;
-      }
-      else
-      {
-        $index++;
-      }
-    }
-    if ($found)
-    {
-      unset($array[$index]);
-    }
-    return $index;
-  }
 
-  /**
-   * This function is to make a key-value pair array
-   * @param string $file_to_load is the file that load from disk
-   * @return array is the paired array with key-values.
-   */
-  function getPaired_array(string $file_to_load): array
+/**
+ * @param array $array is the array to sort
+ * @param string $word_to_search is the word that search in the array
+ * @return int is the index of the word
+ */
+function search_word(
+  array &$array,
+  string $word_to_search,
+  $part_of_speech
+): int
+{
+  $found = false;
+  $index = 0;
+  while ($index < count($array) && !$found)
   {
-    $paired_array = array();
-    // read the file from disk
-    $total_lines = file($file_to_load, FILE_IGNORE_NEW_LINES);
-    //sort the file in ascending order
-    asort($total_lines);
-    $loop = 0;
-    while ($loop < count($total_lines))
+    if (
+      strcmp($array[$index]['word'], $word_to_search) == 0
+      && strcmp($array[$index]['part'], $part_of_speech) == 0
+    )
     {
-      list($word, $part, $definition) = explode("\t", $total_lines[$loop]);
-      $paired_array[] =
-        array('word' => $word, 'part' => $part, 'definition' => $definition);
-      $loop++;
-    }
-    unset($total_lines[$loop]);
-    return $paired_array;
-  }
-
-  /**
-   * This function is to delete the word
-   * @param string $file_to_load is the file that load from disk
-   * @param string $delete_word is the word that user want to delete
-   */
-  function delete_word(string $file_to_load, string $delete_word, $part_of_speech)
-  {
-    $paired_array = getPaired_array($file_to_load);
-    // Locate the word to be deleted
-    $position = search_word($paired_array, $delete_word, $part_of_speech);
-    if ($position == count($paired_array))
-    {
-?>
-      <p>
-        <?= 'No entries found!';?>
-      </p>
-<?php
+      $found = true;
     }
     else
     {
-      unset($paired_array[$position]);
+      $index++;
     }
-    output_to_file($paired_array);
   }
+  if ($found)
+  {
+    unset($array[$index]);
+  }
+  return $index;
+}
+
+/**
+ * This function is to make a key-value pair array
+ * @param string $file_to_load is the file that load from disk
+ * @return array is the paired array with key-values.
+ */
+function getPaired_array(string $file_to_load): array
+{
+  $paired_array = array();
+  // read the file from disk
+  $total_lines = file($file_to_load, FILE_IGNORE_NEW_LINES);
+  //sort the file in ascending order
+  asort($total_lines);
+  $loop = 0;
+  while ($loop < count($total_lines))
+  {
+    list($word, $part, $definition) = explode("\t", $total_lines[$loop]);
+    $paired_array[] = array('word' => $word, 'part' => $part, 'definition' => $definition);
+    $loop++;
+  }
+  unset($total_lines[$loop]);
+  return $paired_array;
+}
+
+/**
+ * This function is to delete the word
+ * @param string $file_to_load is the file that load from disk
+ * @param string $delete_word is the word that user want to delete
+ */
+function delete_word(
+  string $file_to_load,
+  string $delete_word,
+  $part_of_speech
+)
+{
+  $paired_array = getPaired_array($file_to_load);
+  // Locate the word to be deleted
+  $position = search_word($paired_array, $delete_word, $part_of_speech);
+  if ($position == count($paired_array))
+  {
+  ?>
+    <p>
+      <?= 'No entries found!'; ?>
+    </p>
+<?php
+  }
+  else
+  {
+    unset($paired_array[$position]);
+  }
+  output_to_file($paired_array);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <title> Manage Words </title>
-    <meta name="author" content="Hengyi Li" />
-    <link rel="stylesheet" href="managewords.css"/>
-  </head>
-  <body>
+
+<head>
+  <meta charset="utf-8" />
+  <title> Manage Words </title>
+  <meta name="author" content="Hengyi Li" />
+  <link rel="stylesheet" href="managewords.css" />
+</head>
+
+<body>
   <h1 class="header_topic"> Word Manager </h1>
   <p>
     This is the Zelda's GRE vocabulary manager, choose to add a new word or
@@ -171,48 +185,73 @@ function display ( string $file_to_load)
     <p>
       <label for="speech"> What's the part of speech of the word? </label>
       <select name="speech" id="speech">
-        <option value="noun"> noun</option>
-        <option value="adjective"> adjective</option>
-        <option value="adverb"> adverb</option>
-        <option value="verb"> verb</option>
+        <option value=" "> Make a choice </option>
+        <?php
+          $part_of_speech_list = file(PART_OF_SPEECH, FILE_IGNORE_NEW_LINES);
+          $index = 0;
+          while ($index < count($part_of_speech_list))
+          {
+            ?>
+            <option value="<?= $part_of_speech_list[$index] ?>">
+              <?= $part_of_speech_list[$index] ?>
+            </option>
+            <?php
+            $index++;
+          }
+        ?>
       </select>
+    </p>
+    <p>
+      <label for="new_part_speech">
+        Not found you want? Fill your own part of speech below!
+      </label>
+      <input type="text" id="new_part_speech" name="new_part_speech" />
     </p>
     <p>
       <label for="def_new_word"> What's the definition of the word? </label>
       <input type='text' id="def_new_word" name="def_new_word" required />
     </p>
     <p>
-      <input type='submit' value='Submit Report' name='submit' />
+      <input type="submit" value="Submit Report" name="submit" />
     </p>
   </form>
   <?php
-    $paired_array = getPaired_array(DEFINITION_FILENAME);
-    if ( isset($_POST) && isset($_POST['new_word'])
-      && preg_match('|^[A-Za-z^<>]+$|', $_POST['new_word'])
-      && isset($_POST['def_new_word'])
-      && preg_match('|^[A-Za-z;( -^<>]+|', $_POST['def_new_word']))
+  $paired_array = getPaired_array(DEFINITION_FILENAME);
+  if (
+    isset($_POST)
+    && isset($_POST['new_word'])
+    && preg_match('|^[A-Za-z]+$|', $_POST['new_word'])
+    && isset($_POST['new_part_speech'])
+    && preg_match('|^[A-Za-z.]|',  $_POST['new_part_speech'])
+    && isset($_POST['def_new_word'])
+    && preg_match('|^[A-Za-z;( -]+|', $_POST['def_new_word'])
+  )
+  {
+    $word_new = htmlspecialchars($_POST['new_word']);
+    $part_speech = $_POST['speech'];
+    $new_part_speech = htmlspecialchars($_POST['new_part_speech']) . PHP_EOL;
+    $definition = htmlspecialchars($_POST['def_new_word']);
+    $lowercase_word = strtolower($word_new);
+    file_put_contents(PART_OF_SPEECH, $new_part_speech, LOCK_EX | FILE_APPEND);
+    // Make sure there's no duplicate entry
+    $position = search_word($paired_array, $word_new, $part_speech);
+    if ($position < count($paired_array))
     {
-      $word_new = htmlspecialchars($_POST['new_word']);
-      $part_speech = $_POST['speech'];
-      $definition = htmlspecialchars($_POST['def_new_word']);
-      $lowercase_word = strtolower($word_new);
-      // Make sure there's no duplicate entry
-      $position = search_word($paired_array, $word_new, $part_speech);
-      if ($position < count($paired_array))
-      {
   ?>
-        <p>
-          <?= 'The entry has already exists'; ?>
-        </p>
+      <p>
+        <?= 'The entry has already exists'; ?>
+      </p>
   <?php
-      }
-      else
-      {
-        $paired_array[] = array('word' => $lowercase_word, 'part' =>
-        $part_speech, 'definition' => $definition);
-        output_to_file($paired_array);
-      }
     }
+    else
+    {
+      $paired_array[] = array(
+        'word' => $lowercase_word, 'part' => $part_speech,
+        'definition' => $definition
+      );
+      output_to_file($paired_array);
+    }
+  }
   ?>
   <hr />
   <br />
@@ -222,18 +261,20 @@ function display ( string $file_to_load)
       <input type='submit' value='Confirm to delete' name='delete' />
     </p>
     <?php
-      if (isset($_POST) && isset($_POST['choice_to_delete']))
+    if (isset($_POST) && isset($_POST['choice_to_delete']))
+    {
+      $deleted_word_list = $_POST["choice_to_delete"];
+      for ($index = 0; $index < count($deleted_word_list); $index++)
       {
-        $deleted_word_list = $_POST["choice_to_delete"];
-        for ($index = 0; $index < count($deleted_word_list); $index++)
-        {
-          $word_to_be_delete = $paired_array[$deleted_word_list[$index][0]]['word'];
-          $part_of_speech_to_delete = $paired_array[$deleted_word_list[$index][0]]['part'];
-          delete_word(DEFINITION_FILENAME, $word_to_be_delete, $part_of_speech_to_delete);
-        }
+        $word_to_be_delete =
+          $paired_array[$deleted_word_list[$index][0]]['word'];
+        $part_of_speech_to_delete =
+          $paired_array[$deleted_word_list[$index][0]]['part'];
+        delete_word(DEFINITION_FILENAME, $word_to_be_delete, $part_of_speech_to_delete);
+      }
     }
     display(DEFINITION_FILENAME);
     ?>
   </form>
-  </body>
+</body>
 </html>
