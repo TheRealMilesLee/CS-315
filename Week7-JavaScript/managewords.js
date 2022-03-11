3191e4d3075efd1c41e85686d7f7aa125d50d0c3
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-03-11 17:41:22 -0600

    Update the speech detection

--- Week7-JavaScript/managewords.js
+++ Week7-JavaScript/managewords.js
@@ -2,8 +2,17 @@
 var add_submit_button = document.getElementById("new_submit_button");
 var input_words = document.getElementById("new_word");
 var input_definition = document.getElementById("def_new_word");
-var input_speech = document.getElementById("def_new_word")
+var input_speech = document.getElementById("speech");
+var input_new_speech = document.getElementById("new_part_speech");
 input_words.onchange = function ()
+{
+  add_submit_button.disabled = form_validation(this);
+}
+input_speech.onchange = function ()
+{
+  add_submit_button.disabled = form_validation(this);
+}
+input_new_speech.onchange = function ()
 {
   add_submit_button.disabled = form_validation(this);
 }
@@ -13,16 +22,22 @@ input_definition.onchange = function ()
 }
 function form_validation(form)
 {
-  if (document.getElementById("new_word").value === "")
+  if (input_words.value === "")
   {
     window.alert("Please input  the word you want add");
     form.new_word.focus();
     return true;
   }
-  else if (document.getElementById("def_new_word").value === "")
+  else if (input_speech.value === " " && input_new_speech === "")
+  {
+    window.alert("Please select the speech or input your new part of speech");
+    form.speech.focus();
+    return true;
+  }
+  else if (input_definition.value === "")
   {
     window.alert("Please input  the definition you want add");
-    form.definition.focus();
+    form.def_new_word.focus();
     return true;
   }
   else
