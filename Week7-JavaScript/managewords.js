7d5c28a565bcfdce62b90702c5b9c5095763bba5
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-03-11 23:15:20 -0600

    Update the words

--- Week7-JavaScript/managewords.js
+++ Week7-JavaScript/managewords.js
@@ -4,26 +4,11 @@ var input_words = document.getElementById("new_word");
 var input_definition = document.getElementById("def_new_word");
 var input_speech = document.getElementById("speech");
 var input_new_speech = document.getElementById("new_part_speech");
-input_words.onchange = function ()
+window.onchange = function ()
 {
   add_submit_button.disabled = form_validation(this);
 }
-input_speech.onchange = function ()
-{
-  add_submit_button.disabled = form_validation(this);
-}
-input_speech.onchange = function ()
-{
-  add_submit_button.disabled = form_validation(this);
-}
-input_new_speech.onchange = function ()
-{
-  add_submit_button.disabled = form_validation(this);
-}
-input_definition.onchange = function ()
-{
-  add_submit_button.disabled = form_validation(this);
-}
+
 function form_validation(form)
 {
   if (input_words.value === "")
@@ -34,12 +19,14 @@ function form_validation(form)
   }
   else if (input_speech.value === "" && input_new_speech.value === "")
   {
-    window.alert("Have you check your butthole?");
+    window.alert("Please input the speech either by select the current or input a new one");
+    form.speech.focus();
     return true;
   }
   else if (input_definition.value === "")
   {
     window.alert("Please input  the definition you want add");
+    form.def_new_word.focus();
     return true;
   }
   else
