e58940d0359ab488c786f80e7f4c68b01052d44c
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-03-27 12:35:33 -0500

    rearrange the cs

--- Week8-AJAX/managewords.php
+++ Week8-AJAX/managewords.php
@@ -10,8 +10,8 @@
 
 <body>
   <div id="intro_header">
-    <h1 id="word_manager_head"> Word Manager </h1>
-    <p class="chatty_talking">
+    <h1 id="word_manager_h1"> Word Manager </h1>
+    <p id="welcome_prompt">
       This is the Zelda' s GRE vocabulary manager, welcome. <br />
       Go to the left and fill in the blank to add the word, go down below
       and select your choice to delete the words. <br />
@@ -20,16 +20,16 @@
   </div>
   <div id="add_word">
     <!-- Add word section -->
-    <p class="sub-title"> Add a new word </p>
+    <p class="subtitle"> Add a new word </p>
     <!-- Add the new word -->
-    <div id="new_word">
+    <div>
       <p>
         <label for="new_word"> What's the word? </label>
         <input type="text" id="new_word" name="new_word" />
       </p>
     </div>
     <!-- Add the part of speech -->
-    <div id="new_id">
+    <div>
       <p>
         <label for="speech"> What's the part of speech of the word? </label>
         <select name="speech" id="speech">
@@ -42,14 +42,14 @@
       </p>
     </div>
     <!-- Add the definition -->
-    <div id="new_definition">
+    <div>
       <p>
         <label for="def_new_word">What's the definition of the word?</label>
         <input type='text' id="def_new_word" name="def_new_word" />
       </p>
     </div>
     <!-- Add word submit button -->
-    <div id="add_submit">
+    <div>
       <p>
         <input type="submit" id="add_button" value="Add word" name="submit" disabled />
       </p>
@@ -57,8 +57,8 @@
   </div>
   <!-- Delete word section -->
   <div id="del_word">
-    <p class="sub-title"> Delete a word </p>
-    <p id="delete_section_limit_window"> This is the delete part </p>
+    <p class="subtitle"> Delete a word </p>
+    <p id="delete_section_limit_window"></p>
     <!-- Delete word button -->
     <div id="del_submit">
       <p>
