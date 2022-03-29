7ec2636ef3c611e45a0c74e6c2ee41de66982ffa
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-03-29 13:18:05 -0500

    Finished the find fuplicate

--- Week8-AJAX/managewords.js
+++ Week8-AJAX/managewords.js
@@ -30,23 +30,12 @@ get_by_id("add_button").onclick = function ()
  */
 get_by_id("add_word").onchange = function ()
 {
-  clear_delete();
   new_word_validate();
   speech_validate();
   new_definition_validate();
-  let word_with_part =
-      get_by_id("new_word").value + get_by_id("speech").value;
+  let word_with_part = get_by_id("new_word").value + get_by_id("speech").value;
   get_by_id("add_button").disabled = form_validation_add();
   duplicate_validation(word_with_part);
-};
-
-/**
- * This is control the delete section for validation
- */
-get_by_id("display_word").onchange = function ()
-{
-  clear_add();
-  get_by_id("del_button").disabled = form_validation_delete();
 };
 
 /**
@@ -70,6 +59,23 @@ function new_word_validate()
     get_by_id("prompt_user_validate_word").innerHTML = "&check;";
   }
 };
+/**
+ * This function validate the speech
+ */
+function speech_validate()
+{
+  if (get_by_id("speech").value !== "")
+  {
+    get_by_id("prompt_user_validate_speech").style["color"] = "green";
+    get_by_id("prompt_user_validate_speech").innerHTML = "&check;";
+  }
+  else
+  {
+    get_by_id("prompt_user_validate_speech").style["color"] = "red";
+    get_by_id("prompt_user_validate_speech").innerHTML = "&excl;";
+  }
+}
+
 
 /**
  * This function validate the new definition
@@ -81,19 +87,13 @@ function new_definition_validate()
     get_by_id("prompt_user_validate_def").style["color"] = "green";
     get_by_id("prompt_user_validate_def").innerHTML = "&check;";
   }
+  else
+  {
+    get_by_id("prompt_user_validate_def").style["color"] = "red";
+    get_by_id("prompt_user_validate_def").innerHTML = "&excl;";
+   }
 }
 
-/**
- * This function validate the speech
- */
-function speech_validate()
-{
-  if (get_by_id("speech").value !== "")
-  {
-    get_by_id("prompt_user_validate_speech").style["color"] = "green";
-    get_by_id("prompt_user_validate_speech").innerHTML = "&check;";
-  }
-}
 
 /**
  * This is to find the duplicate in the entry
@@ -140,27 +140,6 @@ function form_validation_add()
 }
 
 /**
- * This function is to determine whether the checkbox is checked or not
- * @returns true if the delete check box is empty,
- * return false if the checkbox is not empty
- */
-function form_validation_delete()
-{
-  let index = 0;
-  let empty = true;
-  let paired_array = get_by_name("choice_to_delete[]");
-  while (index < paired_array.length)
-  {
-    if (paired_array[index].checked === true)
-    {
-      empty = false;
-    }
-    index++;
-  }
-  return empty;
-}
-
-/**
  * This function is to clean the addition
  */
 function clear_add()
@@ -170,18 +149,6 @@ function clear_add()
   get_by_id("def_new_word").value = "";
 }
 
-/**
- * This function is to clear the delete section
- */
-function clear_delete()
-{
-  let index = 0;
-  while (index < get_by_name("choice_to_delete[]").length)
-  {
-    get_by_name("choice_to_delete[]")[index].checked = false;
-    index++;
-  }
-}
 
 /**
  * This function is to find the duplication
@@ -189,19 +156,14 @@ function clear_delete()
  */
 function find_duplicate(compare_string)
 {
-  let word_array = [];
+  let word_array;
+  let split_array = [];
   let index = 0;
   while (index < get_by_tag("span").length)
   {
-    word_array.push(get_by_tag("span")[index].innerHTML);
+    word_array = get_by_tag("span")[index].innerHTML.split("\t");
+    split_array.push(word_array);
     index++;
-  }
-  let split_array = [];
-  for (let loop = 0; loop < word_array.length; loop++)
-  {
-    let temp_array = word_array[loop].split("\t");
-    split_array.push(temp_array);
-    loop++;
   }
   for (let looptimes = 0; looptimes < split_array.length; looptimes++)
   {
@@ -217,7 +179,6 @@ function find_duplicate(compare_string)
 
 /**
  * This function is to get the file from the disk
- * @returns {array} is the array of words speech and definitions
  */
 function load_words_from_disk()
 {
@@ -232,22 +193,19 @@ function load_words_from_disk()
     display(response);
   };
   xhr.send();
-
-  return response.length;
 }
 
 function display(response)
 {
-  let temp = response.length;
-  console.log(temp);
   for (let index = 0; index < response.length; index++)
   {
-    let new_word_line = document.createElement("p");
-    let word = document.createTextNode(index + " : " + response[index][0] + "\t" + response[index][1] + "\t" + response[index][2] + "\n");
-    console.log(word);
+    let new_word_line = document.createElement("span");
+    let word = document.createTextNode(response[index][0] + "\t" + response[index][1] + "\t" + response[index][2]);
     new_word_line.appendChild(word);
     new_word_line.classList.add("word_list");
     let original_div = get_by_id("display");
+    let newline = document.createElement("p");
     original_div.appendChild(new_word_line);
+    original_div.appendChild(newline);
   }
 }
