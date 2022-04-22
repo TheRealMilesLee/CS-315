8b24f5e5d9ba0d7ed7cf551e9cba98db5ea959e0
Hengyi Li <hl3265@vh222004.truman.edu>
2022-04-21 22:12:33 -0500

    deleted:    Week9-SQL/.vscode/ltex.dictionary.en-US.txt 	deleted:    Week9-SQL/.vscode/ltex.hiddenFalsePositives.en-US.txt 	deleted:    Week9-SQL/.vscode/settings.json 	deleted:    Week9-SQL/add_word.php 	deleted:    Week9-SQL/create_insert.py 	deleted:    Week9-SQL/create_part.py 	deleted:    Week9-SQL/dblogin.php 	deleted:    Week9-SQL/delete_word.php 	deleted:    Week9-SQL/delete_words.php 	deleted:    Week9-SQL/favicon.ico 	deleted:    Week9-SQL/get_words.php 	deleted:    Week9-SQL/insert.sql 	deleted:    Week9-SQL/logger.txt 	deleted:    Week9-SQL/managewords.css 	deleted:    Week9-SQL/managewords.html 	deleted:    Week9-SQL/managewords.js 	deleted:    Week9-SQL/part.sql 	deleted:    Week9-SQL/tables.sql 	deleted:    Week9-SQL/url.txt 	deleted:    Week9-SQL/words.txt

--- Week9-SQL/part.sql
+++
@@ -1,5 +0,0 @@
-use hl3265;
-insert into part (part) values ('adjective');
-insert into part (part) values ('adverb');
-insert into part (part) values ('noun');
-insert into part (part) values ('verb');
