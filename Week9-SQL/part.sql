a5d473ddd971d60850cfaf126648015daa90e2b5
Hengyi Li <hl3265@vh222004.truman.edu>
2022-04-21 22:13:21 -0500

    new file:   Week9-SQL/.vscode/ltex.dictionary.en-US.txt 	new file:   Week9-SQL/.vscode/ltex.hiddenFalsePositives.en-US.txt 	new file:   Week9-SQL/.vscode/settings.json 	new file:   Week9-SQL/add_word.php 	new file:   Week9-SQL/create_insert.py 	new file:   Week9-SQL/create_part.py 	new file:   Week9-SQL/dblogin.php 	new file:   Week9-SQL/delete_words.php 	new file:   Week9-SQL/favicon.ico 	new file:   Week9-SQL/get_words.php 	new file:   Week9-SQL/insert.sql 	new file:   Week9-SQL/logger.txt 	new file:   Week9-SQL/managewords.css 	new file:   Week9-SQL/managewords.html 	new file:   Week9-SQL/managewords.js 	new file:   Week9-SQL/part.sql 	new file:   Week9-SQL/tables.sql 	new file:   Week9-SQL/url.txt 	new file:   Week9-SQL/words.txt

---
+++ Week9-SQL/part.sql
@@ -0,0 +1,5 @@
+use hl3265;
+insert into part (part) values ('adjective');
+insert into part (part) values ('adverb');
+insert into part (part) values ('noun');
+insert into part (part) values ('verb');
