ef5d1b37d96354c9ffa105d5d60a7445e9d89328
Hengyi Li <hl3265@vh222004.truman.edu>
2022-04-24 16:45:52 -0500

    new file:   Week10-Session/Login.jpg 	modified:   Week10-Session/database_management_system.css 	modified:   Week10-Session/database_management_system.php

--- Week10-Session/database_management_system.php
+++ Week10-Session/database_management_system.php
@@ -10,19 +10,24 @@
 </head>
 
 <body>
-  <div class="login_art">
-    <img src="smiley.gif" alt="Smiley face" width="42" height="42" />
+  <div class="prompt">
+    <h1>Login</h1>
+    <p> By type in the user name and the password, you will be redirected to the home page</p>
   </div>
-    <div class="login">
-      <fieldset id="username_login">
-        <label class="login_input_label" for=" username">Please input your username </label>
-        <input class="login_input" type="text" id="username" name="username" placeholder="Username" required />
-      </fieldset>
-      <fieldset id="password_login">
-        <label class="login_input_label" for=" password">Please input your Password </label>
-        <input class="login_input" type="text" id="password" name="password" placeholder="Password" required />
-      </fieldset>
-    </div>
+  <div class="login">
+    <fieldset id="username_login">
+      <label class="login_input_label" for=" username">Please input your username </label>
+      <input class="login_input" type="text" id="username" name="username" placeholder="Username" required />
+    </fieldset>
+    <br />
+    <fieldset id="password_login">
+      <label class="login_input_label" for=" password">Please input your Password </label>
+      <input class="login_input" type="text" id="password" name="password" placeholder="Password" required />
+    </fieldset>
+    <br />
+    <button type="submit" id="login_submit" name="login_submit"> Log In </button>
+    <br />
+  </div>
 </body>
 
 </html>
