cdd3e07d61a2111434802f81b14c6a27cac2a222
Hengyi Li <hl3265@vh222004.truman.edu>
2022-04-24 16:00:53 -0500

    modified:   Week10-Session/database_management_system.css 	deleted:    Week10-Session/database_management_system.html 	new file:   Week10-Session/database_management_system.php

---
+++ Week10-Session/database_management_system.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title> Database Management System </title>
+  <meta name="author" content="Hengyi Li" />
+  <link rel="icon" href="datacenter.png">
+  <link rel="stylesheet" href="database_management_system.css" />
+</head>
+
+<body>
+  <div class="login_art">
+    <img src="smiley.gif" alt="Smiley face" width="42" height="42" />
+  </div>
+    <div class="login">
+      <fieldset id="username_login">
+        <label class="login_input_label" for=" username">Please input your username </label>
+        <input class="login_input" type="text" id="username" name="username" placeholder="Username" required />
+      </fieldset>
+      <fieldset id="password_login">
+        <label class="login_input_label" for=" password">Please input your Password </label>
+        <input class="login_input" type="text" id="password" name="password" placeholder="Password" required />
+      </fieldset>
+    </div>
+</body>
+
+</html>
