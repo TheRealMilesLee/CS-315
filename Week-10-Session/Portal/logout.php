ffeed58c762498acc25a8be4caae1fdd8c24a5a1
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-05-05 22:15:44 -0500

    modified:   Week-10-Session/Portal/logout.php

--- Week-10-Session/Portal/logout.php
+++ Week-10-Session/Portal/logout.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 
-$_SESSION = array();
+$_SESSION = array(); //Clean all the data from the session
 if (session_id() != '' || isset($_COOKIE[session_name()]))
 {
   setcookie(session_name(), '', time() - 3600, '/');
 }
 session_destroy();
-
 header('Location: managewords_portal_login.php');
+?>
